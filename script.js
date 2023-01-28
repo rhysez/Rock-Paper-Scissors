a5f68e6b4d9d2a6ffe40f87e8ec4704e979ff1e3
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-01-28 21:18:02 +0000

    No significant changes made. Unused functions removed.

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-//All available choices for game
+//All available choices for game //
 const choices = ["rock", "paper", "scissors"] 
 
+
 function game(){
-    playRound();    
+    console.log("Make your choice");
+    playRound();
 }
 
-// Calling this function randomly returns either "rock" "raper" or "rcissors" from the computer
+// Starts a round //
+function playRound(playerSelection, computerSelection){ 
+    
+}
+
+// Calling this function randomly returns either "rock" "raper" or "Scissors" from the computer //
 function getComputerChoice(){
     let compChoice = choices[Math.floor(Math.random() * choices.length)]
     return "The computer chose: " + compChoice; 
 }   
 
-// Gets input from the player
 function getPlayerChoice(){
-    let input = prompt("Enter rock, paper or scissors!")
-    input = input.toLowerCase;
-    input = input.toUpperCase; //Makes the input case insensitive
-}   
-
-// Starts a round
-function playRound(){
-    const playerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
+   
 }
 
+
+game();
 
 
 
