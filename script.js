ca8d49a0e5a7c056ac5dec2453433f8716e5c6e6
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-01-31 22:31:35 +0000

    added playerScore and computerScore variables to keep track of score

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-
+//Calls the playRound function
 function game(){
-    playRound("rock", getComputerChoice());
+    playRound()
 }
 
-// Calling this function randomly returns either "rock" "paper" or "scissors" from the computer //
+// Calling this function randomly returns either "rock" "paper" or "scissors" from the computer 
 function getComputerChoice(){
     const pick = ["rock", "paper", "scissors"]
     return pick[Math.floor(Math.random() * pick.length)]
 }  
 
-// Starts a round //
+// Starts a round and decides a win, loss or tie
 function playRound(playerSelection, computerSelection){ 
     if (playerSelection === computerSelection) {
         return "It's a tie, try again!"
-    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
-        playerSelection === "scissors" && computerSelection === "paper" ||
-        playerSelection === "paper" && computerSelection === "rock") {
-            return "You win!"
-        } else if (playerSelection === "scissors" && computerSelection === "rock" ||
-                    playerSelection === "paper" && computerSelection === "scissors" ||
-                    playerSelection === "rock" && computerSelection === "paper") {
-                        return "Computer wins!"
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" ||
+        playerSelection.toLowerCase() === "scissors" && computerSelection === "paper" ||
+        playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+            playerScore++;
+            return `You win! Your score is ${playerScore}!`
+        } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock" ||
+                    playerSelection.toLowerCase() === "paper" && computerSelection === "scissors" ||
+                    playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+                        computerScore++;
+                        return `Computer wins! Computer score is ${computerScore}`
                     } 
 }
 
-const playerSelection = prompt("Rock, paper or scissors?")
+//Variables for player choice and computer choice
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
+const playerSelection = prompt("Rock, paper or scissors?");
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection))
 
