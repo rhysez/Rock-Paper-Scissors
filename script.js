828216a984a1e79d4ac39cad7b26d971d7609944
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-01-31 23:32:49 +0000

    for loop added to integrate 5 round system and keep track of scores

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-//Calls the playRound function
+// Calls the playRound function
 function game(){
     playRound()
 }
 
-// Calling this function randomly returns either "rock" "paper" or "scissors" from the computer 
+// Randomly returns either "rock" "paper" or "scissors" from the computer 
 function getComputerChoice(){
     const pick = ["rock", "paper", "scissors"]
     return pick[Math.floor(Math.random() * pick.length)]
 }  
 
 // Starts a round and decides a win, loss or tie
+// playerScore++ and computerScore++ will keep tally of scores
 function playRound(playerSelection, computerSelection){ 
     if (playerSelection === computerSelection) {
         return "It's a tie, try again!"
@@ -17,21 +18,33 @@ function playRound(playerSelection, computerSelection){
         playerSelection.toLowerCase() === "scissors" && computerSelection === "paper" ||
         playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
             playerScore++;
+            overallScore++;
             return `You win! Your score is ${playerScore}!`
         } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock" ||
                     playerSelection.toLowerCase() === "paper" && computerSelection === "scissors" ||
                     playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
                         computerScore++;
+                        overallScore++;
                         return `Computer wins! Computer score is ${computerScore}`
                     } 
 }
 
-//Variables for player choice and computer choice
+// Variables for player choice and computer choice
 let playerScore = parseInt(0);
 let computerScore = parseInt(0);
-const playerSelection = prompt("Rock, paper or scissors?");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
+let overallScore = playerScore + computerScore;
+
+for (i = 0; i < 5; i++){
+    const playerSelection = prompt("Rock, paper or scissors?");
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection))
+} if (i === 5 && playerScore > computerScore){
+    console.log("You have won! Congratulations!")
+} else if (i === 5 && playerScore < computerScore){
+    console.log("You lose! Better luck next time.")
+} else if (i === 5 && playerScore === computerScore){
+    console.log("It's a tie! Is a rematch on the horizon?")
+}
 
 game();
 
