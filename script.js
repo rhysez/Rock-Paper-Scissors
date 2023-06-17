5bb52dcb52dc4eaddc16a8ae7fb3d1d35b19fee0
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-06-17 13:54:29 +0100

    added gameOver() win condition, format code using prettier

--- script.js
+++ script.js
@@ -1,86 +1,93 @@
 // html references
-const buttonRock = document.querySelector('#rock');
-const buttonPaper = document.querySelector('#paper');
-const buttonScissors = document.querySelector('#scissors');
-const container = document.querySelector('#container');
-const scoreTally = document.createElement('div');
-const winnerLoser = document.createElement('div');
-scoreTally.classList.add('scoretally');
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+const container = document.querySelector("#container");
+const scoreTally = document.createElement("div");
+const winnerLoser = document.createElement("div");
+scoreTally.classList.add("scoretally");
 scoreTally.textContent = "You will see your scores here.";
 scoreTally.style.fontSize = "xx-large";
 scoreTally.style.fontWeight = "bold";
 scoreTally.style.color = "white";
-winnerLoser.classList.add('winnerloser');
-winnerLoser.textContent = ""
+winnerLoser.classList.add("winnerloser");
+winnerLoser.textContent = "";
 
 // Selects player choice and alerts outcome
-buttonRock.addEventListener('click', function(){
-    playRound("rock", getComputerChoice())
+buttonRock.addEventListener("click", function () {
+  playRound("rock", getComputerChoice());
 });
 
-buttonPaper.addEventListener('click', function(){
-    playRound("paper", getComputerChoice())
+buttonPaper.addEventListener("click", function () {
+  playRound("paper", getComputerChoice());
 });
 
-buttonScissors.addEventListener('click', function(){
-    playRound("scissors", getComputerChoice())
+buttonScissors.addEventListener("click", function () {
+  playRound("scissors", getComputerChoice());
 });
-
 
 // Variables for player choice and computer choice
 let playerScore = parseInt(0);
 let computerScore = parseInt(0);
 let overallScore = playerScore + computerScore;
 
-
 // Randomly returns either "rock" "paper" or "scissors" from the computer
-function getComputerChoice(){
-    const pick = ["rock", "paper", "scissors"]
-    return pick[Math.floor(Math.random() * pick.length)]
-}  
-
-function pickSame(){
-    scoreTally.textContent = "Oof, you both picked the same!"
-    scoreTally.style.color = "white"
+function getComputerChoice() {
+  const pick = ["rock", "paper", "scissors"];
+  return pick[Math.floor(Math.random() * pick.length)];
 }
 
-function pickWin(){
-    playerScore++;
-    overallScore++;
-    scoreTally.textContent = `You win! Your score: ${playerScore}, Computer score: ${computerScore}`
-    scoreTally.style.color = "gold"
+function pickSame() {
+  scoreTally.textContent = "Oof, you both picked the same!";
+  scoreTally.style.color = "white";
 }
 
-function pickLose(){
-    computerScore++;
-    overallScore++;
-    scoreTally.textContent = `Computer wins! Your score: ${playerScore}, Computer score: ${computerScore}`
-    scoreTally.style.color = "red"
+function pickWin() {
+  playerScore++;
+  scoreTally.textContent = `You win! Your score: ${playerScore}, Computer score: ${computerScore}`;
+  scoreTally.style.color = "gold";
+}
+
+function pickLose() {
+  computerScore++;
+  scoreTally.textContent = `Computer wins! Your score: ${playerScore}, Computer score: ${computerScore}`;
+  scoreTally.style.color = "red";
+}
+
+function gameOver() {
+  scoreTally.textContent = `Game over! Your score: ${playerScore}, Computer score: ${computerScore}`;
+  playerScore = parseInt(0);
+  computerScore = parseInt(0);
 }
 
 // Starts a round and decides a win, loss or tie
 // playerScore++ and computerScore++ will keep tally of scores
-function playRound(playerSelection, computerSelection){ 
-    if (playerSelection === computerSelection) { 
-        pickSame() 
-    }
-    if (playerSelection === "rock" && computerSelection === "scissors" ||
-        playerSelection === "scissors" && computerSelection === "paper" ||
-        playerSelection === "paper" && computerSelection === "rock") {
-        pickWin()
-    } 
-    if (playerSelection === "scissors" && computerSelection === "rock" ||
-        playerSelection === "paper" && computerSelection === "scissors" ||
-        playerSelection === "rock" && computerSelection === "paper") {
-        pickLose()
-    } 
+// Game ends at 5 points
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    pickSame();
+  }
+
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    pickWin();
+  }
+
+  if (
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "rock" && computerSelection === "paper")
+  ) {
+    pickLose();
+  }
+
+  if (playerScore === parseInt(5) || computerScore === parseInt(5)) {
+    gameOver();
+  }
 }
 
 container.appendChild(scoreTally);
 container.appendChild(winnerLoser);
-
-
-
-
-
-
